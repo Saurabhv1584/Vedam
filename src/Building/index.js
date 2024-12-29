f309a527a3fcdc019be63222d0050e47eb44c01b
Saurabh <saurabhv1584@gmail.com>
2024-12-29 18:19:23 +0530

    building modified

--- src/Building/index.js
+++ src/Building/index.js
@@ -10,7 +10,7 @@ const Building = () => {
       <Navbar />
       <div className="flex flex-col justify-center">
         <img
-          className="h-calc(100vh - 1.2*30vh) sm:h-[calc(100vh-20vh)] md:h-[calc(100vh-10vh)] p-4"
+          className="h-calc(100vh - 1.2*30vh) sm:h-[calc(100vh-20vh)] md:h-[calc(100vh-10vh)] "
           src="https://maribellevue.wpengine.com/wp-content/uploads/Dreamlife_Mira2_Cam2_Exterior_Lake_Final-1.jpg"
           alt="building"
         />
@@ -46,16 +46,16 @@ const Building = () => {
         </div>
       </div>
 
-      <div className=" mx-auto px-4 flex items-center box-border mt-6 buildingAnimation">
+      <div className=" mx-auto sm:px-4 flex items-center box-border mt-6 buildingAnimation">
         <img
           src="https://maribellevue.wpengine.com/wp-content/uploads/Dreamlife_Mira2_Cam3_Exterior_CityView_Final-1__1.jpg"
           alt="img"
-          className="ml-auto mr-auto h-[calc(100vh-80px)]"
+          className="ml-auto mr-auto sm:h-[calc(100vh-80px)] h-[50vh]"
         />
       </div>
 
       <div className="mx-auto px-6 lg:mx-auto relative mt-4 xl:px-20 lg:px-16 md:px-12 md:flex items-center gap-8 buildingAnimationRight">
-        <div className="text-color flex justify-center flex-col w-4/5 md:w-1/3 sm:w-4/5 pt-4 sm:px-12 lg:pt-2 xl:pt-10 md:pl-8 md:pr-12 pl-0 pr-0">
+        <div className="text-color flex justify-center flex-col w-4/4 md:w-1/2  pt-4 lg:pt-2 xl:pt-10 md:pl-8 md:pr-12 pl-0 pr-0 text-end md:text-left">
           <h1 className="text-4xl md:text-4xl lg:text-4xl w-11/12 lg:w-11/12 xl:w-full xl:text-4xl text-gray-900 font-extrabold f-f-l">
             Soaring High: Vedam Remarkable Growth
           </h1>
@@ -69,13 +69,13 @@ const Building = () => {
           </div>
         </div>
         <img
-          className="w-full mt-2 sm:mt-6 md:mt-0 object-fill md:w-2/3 md:-ml-4 lg:-ml-4 xl:ml-0"
-          src="https://tuk-cdn.s3.amazonaws.com/can-uploader/Graphic.png"
+          className="w-full mt-4 sm:mt-6 md:mt-0 object-fill md:w-2/4 md:-ml-4 lg:-ml-4 xl:ml-0 buildingAnimationRight md:h-[85vh]"
+          src="banner-2.jpeg"
           alt="Sample"
         />
       </div>
 
-      <div className="lg:px-20 md:px-6 px-4 md:py-12 py-8 buildingAnimationLeft">
+      <div className="lg:px-20 md:px-6 px-4 md:py-12 sm:py-8 py-4 buildingAnimationLeft">
         <div className="lg:flex items-center justify-between">
           <div className="lg:w-7/12 lg:mt-0 mt-8">
             <div className="w-full h-full bg-red-200">
@@ -95,11 +95,11 @@ const Building = () => {
               />
             </div>
           </div>
-          <div className="lg:w-1/3 mt-4">
-            <h1 className="text-4xl font-semibold leading-9 text-gray-800">
-              Indoor Interiors
+          <div className="flex flex-col items-center lg:w-1/3 mt-4">
+            <h1 className="focus:outline-none md:w-60 font-bold md:text-6xl text-4xl leading-tight text-gray-800">
+              Indoor<span className="font-normal italic"> Interiors</span>
             </h1>
-            <p className="text-base leading-6 mt-4 text-gray-600">
+            <p className="text-base leading-6 mt-4 text-center text-gray-600">
               Get inspired by our curated selection of luxiwood interiors. We
               hope get inspired to have luxiwood interior yourself. You’ll find
               tips here where you can buy a lot of cool furniture.
@@ -144,17 +144,17 @@ const Building = () => {
       </div>
 
       <div
-        className="py-4 sm:py:14 md:py:20 2xl:px-0 lg:px-12 px-4 2xl:mx-auto buildingAnimationRight"
+        className="py-4 sm:py:14 md:py:20 2xl:px-0 lg:px-12 sm:px-4 2xl:mx-auto buildingAnimationRight"
         // style={{ height: 1000 }}
       >
-        <div className="md:flex items-center justify-between w-full px-10">
-          <div className="xl:w-1/3 md:w-1/2 w-full">
+        <div className="md:flex items-center justify-between w-full sm:px-8 px-4">
+          <div className="flex flex-col items-center xl:w-1/3 md:w-1/2 w-full">
             <h1 className="focus:outline-none md:w-60 font-bold md:text-6xl text-4xl leading-tight text-gray-800">
               Think<span className="font-normal italic"> out loud </span>
             </h1>
             <p
               role="contentinfo"
-              className="focus:outline-none text-base leading-6 mt-4 sm:mt-8 text-gray-600 2xl:pr-24 xl:pr-0 pr-12"
+              className="focus:outline-none leading-6 mt-4 sm:mt-8 text-gray-600 2xl:pr-24 xl:pr-0 sm:pr-12 text-center"
             >
               Trusted by discerning high-net-worth individuals, Navraj has
               earned a reputation for excellence. Our client testimonials are a
@@ -162,41 +162,41 @@ const Building = () => {
               exceptional living spaces.
             </p>
             <div className="mt-10">
-              <NavLink to='/Amenities' end >
-              <button className="flex items-center md:p-3 p-1 focus:outline-none text-xl font-medium leading-5 text-gray-800 focus:ring-2 focus:ring-offset-2 focus:ring-gray-700">
-                Discover more
-                <svg
-                  className="ml-8"
-                  width={45}
-                  height={40}
-                  viewBox="0 0 45 40"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M11.2113 33.7887C11.0161 33.9839 11.0157 34.3009 11.2157 34.4911C13.956 37.0978 17.3821 38.8765 21.0982 39.6157C24.9778 40.3874 28.9991 39.9913 32.6537 38.4776C36.3082 36.9638 39.4318 34.4004 41.6294 31.1114C43.827 27.8224 45 23.9556 45 20C45 16.0444 43.827 12.1776 41.6294 8.8886C39.4318 5.59961 36.3082 3.03616 32.6537 1.52241C28.9991 0.00865715 24.9778 -0.38741 21.0982 0.384294C17.3821 1.12346 13.956 2.90221 11.2157 5.50886C11.0157 5.69911 11.0161 6.01609 11.2113 6.21127C11.4065 6.40645 11.7225 6.4059 11.9227 6.21592C14.523 3.74892 17.7711 2.06527 21.2932 1.36468C24.9789 0.631541 28.7993 1.00781 32.2711 2.44591C35.743 3.88401 38.7105 6.31934 40.7983 9.44394C42.8861 12.5685 44.0004 16.2421 44.0004 20C44.0004 23.7579 42.8861 27.4315 40.7983 30.5561C38.7105 33.6807 35.743 36.116 32.2711 37.5541C28.7993 38.9922 24.9789 39.3685 21.2932 38.6353C17.7711 37.9347 14.523 36.2511 11.9227 33.7841C11.7225 33.5941 11.4065 33.5935 11.2113 33.7887Z"
-                    fill="#1F2937"
-                  />
-                  <path
-                    d="M1 20H29"
-                    stroke="#1F2937"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M21 28L29 20"
-                    stroke="#1F2937"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M21 12L29 20"
-                    stroke="#1F2937"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-              </button>
+              <NavLink to="/Amenities" end>
+                <button className="flex items-center md:p-3 p-1 focus:outline-none text-xl font-medium leading-5 text-gray-800 focus:ring-2 focus:ring-offset-2 focus:ring-gray-700">
+                  Discover more
+                  <svg
+                    className="ml-8"
+                    width={45}
+                    height={40}
+                    viewBox="0 0 45 40"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M11.2113 33.7887C11.0161 33.9839 11.0157 34.3009 11.2157 34.4911C13.956 37.0978 17.3821 38.8765 21.0982 39.6157C24.9778 40.3874 28.9991 39.9913 32.6537 38.4776C36.3082 36.9638 39.4318 34.4004 41.6294 31.1114C43.827 27.8224 45 23.9556 45 20C45 16.0444 43.827 12.1776 41.6294 8.8886C39.4318 5.59961 36.3082 3.03616 32.6537 1.52241C28.9991 0.00865715 24.9778 -0.38741 21.0982 0.384294C17.3821 1.12346 13.956 2.90221 11.2157 5.50886C11.0157 5.69911 11.0161 6.01609 11.2113 6.21127C11.4065 6.40645 11.7225 6.4059 11.9227 6.21592C14.523 3.74892 17.7711 2.06527 21.2932 1.36468C24.9789 0.631541 28.7993 1.00781 32.2711 2.44591C35.743 3.88401 38.7105 6.31934 40.7983 9.44394C42.8861 12.5685 44.0004 16.2421 44.0004 20C44.0004 23.7579 42.8861 27.4315 40.7983 30.5561C38.7105 33.6807 35.743 36.116 32.2711 37.5541C28.7993 38.9922 24.9789 39.3685 21.2932 38.6353C17.7711 37.9347 14.523 36.2511 11.9227 33.7841C11.7225 33.5941 11.4065 33.5935 11.2113 33.7887Z"
+                      fill="#1F2937"
+                    />
+                    <path
+                      d="M1 20H29"
+                      stroke="#1F2937"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M21 28L29 20"
+                      stroke="#1F2937"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M21 12L29 20"
+                      stroke="#1F2937"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </button>
               </NavLink>
             </div>
           </div>
@@ -209,9 +209,9 @@ const Building = () => {
             </p> */}
             <div className="w-full">
               <img
-                src="https://maribellevue.com/wp-content/uploads/mari-1.png"
+                src="11.jpg"
                 alt="Purple flowers on a book"
-                className="md:w-full sm:w-1/2 w-full"
+                className="w-full mt-8 sm:mt-0 "
               />
             </div>
           </div>
@@ -250,39 +250,39 @@ const Building = () => {
                 find tips here where you can buy a lot of cool
                 furniture,decorations, plants, etc.
               </p>
-              <NavLink to='/Enquire' end>
-              <button className="dark:bg-white dark:hover:bg-gray-400 dark:hover:text-gray-100 dark:text-gray-800 sm:w-auto w-full mt-8 text-base justify-between focus:ring-2 focus:ring-offset-2 focus:ring-gray-700 focus:outline-none hover:bg-gray-700 font-medium leading-none text-white py-4 px-8 bg-gray-800 flex items-center">
-                Explore
-                <div className="ml-2 mt-0.5">
-                  <svg
-                    className="fill-stroke"
-                    width="16"
-                    height="16"
-                    viewBox="0 0 16 16"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M3.33325 8H12.6666"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                    <path
-                      d="M10 10.6667L12.6667 8"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                    <path
-                      d="M10 5.33301L12.6667 7.99967"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                </div>
-              </button>
+              <NavLink to="/Enquire" end>
+                <button className="dark:bg-white dark:hover:bg-gray-400 dark:hover:text-gray-100 dark:text-gray-800 sm:w-auto w-full mt-8 text-base justify-between focus:ring-2 focus:ring-offset-2 focus:ring-gray-700 focus:outline-none hover:bg-gray-700 font-medium leading-none text-white py-4 px-8 bg-gray-800 flex items-center">
+                  Explore
+                  <div className="ml-2 mt-0.5">
+                    <svg
+                      className="fill-stroke"
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M3.33325 8H12.6666"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M10 10.6667L12.6667 8"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M10 5.33301L12.6667 7.99967"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </div>
+                </button>
               </NavLink>
             </div>
           </div>
